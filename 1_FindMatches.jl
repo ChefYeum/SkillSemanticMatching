b2d0e71f3097691737f08fa75142635a3be8a662
ChefYeum <chefyeum@gmail.com>
2019-07-23 11:52:32 +0100

    File renamed: compares the SS user sample and LinkedIn skills

---
+++ 1_FindMatches.jl
@@ -0,0 +1,42 @@
+using CSV
+using DataFrames
+include("util.jl")
+#
+# Read all skills and filter non-ASCII strings
+symbaSkills = filterAsciiStrings(linesToArr("data/SymbaSyncSkills"))
+sampleSkills = filterAsciiStrings(linesToArr("data/sampleSkills"))
+linkedinSkills = filterAsciiStrings(linesToArr("data/all_linked_skills.txt"))
+
+global count
+count = 0
+noOfSample = length(sampleSkills)
+
+function mostSimilar(skillName::String)::Array{Tuple{String,Float32}}
+    global count += 1
+    output = Tuple{String,Float32}[] 
+    for s in linkedinSkills
+        println("$count/$noOfSample : $skillName compared with $s")
+        getSimilarity::Float32 = similarity(skillName,s) 
+        if getSimilarity > 0.7
+            push!(output,(s, getSimilarity))
+        end
+    end 
+    return output
+end 
+
+dfSkills = DataFrame(skillName = sampleSkills)
+dfSkills[:exactMatch] = map((skillName) -> (skillName in symbaSkills ? true : false), sampleSkills)
+dfSkills[:matches] = Array{Tuple{String,Float32}}
+dfSkills[:matches] = map((exactMatch, skillName) -> (exactMatch ? skillName : mostSimilar(skillName)), dfSkills[:exactMatch], dfSkills[:skillName])
+
+#Sort the matches by similarity
+dfSkills[:matches] = map( skill -> typeof(skill) == String ? skill : sort(skill, by=s-> s[2],rev=true), f[:matches])
+
+#Save processed data
+CSV.write("matches.csv", dfSkills)
+
+#TO DO
+#1. Get the max similarity for each skill
+#2. Find what percentage a skill results more than one similar skill name
+#3. From the filtered data, figure out a solution to match more skills
+
